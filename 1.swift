eb88faa044c5a06ac8b237b88da8507c1710b695
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-07-20 18:45:39 +0430

    added readme

--- 1.swift
+++ 1.swift
@@ -12,3 +12,22 @@ integer and double variables mixed together
 var num1 = 55.0;
 var num2 = 55.0;
 print(num1 * num2);
+
+//type function in swift
+type(of:a)
+
+//wow explicit type assignment is also possible in swift. interesting!
+var num:Int = 25
+var str:String = "yess!!!"
+
+/*
+this is getting more and more weird!
+in swift, once a variable's type is
+infered, you can't assign a value of a different type 
+to that variable!
+mind blowing!! I haven't ever seen such thing in any other
+programming language. uncommenting the code below will cause a compilation error.
+*/
+var myInt = 10
+// myInt = 5.5
+
---
+++ README.md
@@ -0,0 +1,4 @@
+<img src="https://www.vectorlogo.zone/logos/swift/swift-vertical.svg" width="150"/>
+
+### Learned the basics of Swift programming language in this course
+[Course Link](https://www.udemy.com/course/swift-5-for-absolute-beginners/)
