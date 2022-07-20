5f44a07dacd472c377144773b859c42f7c328025
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-07-20 18:28:45 +0430

    started learning

---
+++ 1.swift
@@ -0,0 +1,14 @@
+// var is the keyword to define a variable in swift.
+var a = 10;
+//the print syntax is similar to dart
+print(a);
+
+/*
+this will result into an error because
+as stupid as it sounds, swift cannot 
+perform mathematical operations with 
+integer and double variables mixed together
+*/
+var num1 = 55.0;
+var num2 = 55.0;
+print(num1 * num2);
