1d2e867b28247110f88f05c72bb44b6cdcf0058b
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-07-21 09:52:11 +0430

    earned my skill badge

---
+++ linkedin.swift
@@ -0,0 +1,28 @@
+// extension String  {
+//     var a: Character = "c" {
+//         didSet {
+//             print("new val")
+//         }
+//     }
+// }
+
+// "t".forEach { (char) in
+// print(char)
+// }
+protocol b{
+
+}
+
+class a:b {
+
+}
+
+
+typealias thing = [String:Any]
+var c: thing;
+print(type(of:c))
+
+let vt = (name: "as",val: 5)
+print(vt.0)
+
+print(["1","2","3"].map{$0})
